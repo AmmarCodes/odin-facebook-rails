adf3c48044ac6439b37abd79b7dc31ca6ec6ab45
Ammar Alakkad <am.alakkad@gmail.com>
2023-10-13 18:13:33 +0300

    Temp remove not-working sanitization

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,4 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   validates :title, presence: true
   validates :content, presence: true
-
-  before_validation :sanitize_inputs
 end
